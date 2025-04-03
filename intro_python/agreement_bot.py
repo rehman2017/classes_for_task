471147039019eddbed03e2877b30b0c224f79103
Engr Sir Abdul Rehman Ansari <166437385+rehman2017@users.noreply.github.com>
2025-04-03 20:09:38 +0500

    my favourite bird

---
+++ intro_python/agreement_bot.py
@@ -0,0 +1,9 @@
+def favorite_bird():
+    # Getting user input and storing it into a variable
+    fav_bird: str = input("What's your favorite bird? ")
+    # Printing my Favorite bird with same Name
+    print(f"\nMy favorite bird is also {fav_bird}!")
+
+
+if __name__ == '__main__':
+    favorite_bird()
